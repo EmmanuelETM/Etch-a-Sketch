467683815233c40375314f2e4f91df96ddd892a1
Emmanuel Torres Malena <torresmalenaemmanuel@gmail.com>
2024-10-17 19:50:23 -0400

    Feat: working on sketchpad functionality

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-let container = document.querySelector('.container');
+for(let i = 0; i < 1003; i++){
+    let div = document.createElement('div');
+    sketchpadDiv.appendChild(div);
+}
+
+sketchpadDiv.addEventListener('mouseover', (Event) => {
+    console.log(Event.target);
+    Event.target.style.backgroundColor = 'gray';
+});
+
+restartButton.addEventListener('click', (Event) => {
+    const cells = document.querySelectorAll('.sketchpad div');
+    cells.forEach(element => {
+        element.style.backgroundColor = 'white';
+    })
+})
